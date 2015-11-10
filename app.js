acc01553916f9796e52d363fa35806decb8be5e8
Jessie Zhang <jessielz@uw.edu>
2015-11-10 04:40:56 -0800

    first commit of my homework assignment

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var url = 'https://api.spotify.com/v1/search?type=track&query='
+
 var myApp = angular.module('myApp', [])
+
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
+  $scope.sortType = 'track'
+  
   $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+    $http.get(url + $scope.track).success(function(response){
+      data = $scope.tracks = response.tracks.items;
     })
   }
-  $scope.play = function(song) {
-    if($scope.currentSong == song) {
-      $scope.audioObject.pause()
-      $scope.currentSong = false
+
+  $scope.play = function(song, data) {
+    $scope.trackName = data.name;
+    $scope.trackArtist = data.artists[0].name;
+    if($scope.currentSong == song) { // playing this song
+      $scope.audioObject.pause() // pauses the song
+      $scope.currentSong = false // then the song is not playing
       return
     }
     else {
@@ -23,9 +29,37 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
+
+// $('img').mouseover(function(){
+//     $(this).css('opacity','.2');
+//     $(this).next('span.text-content').show();
+// }).mouseout(function(){
+//     $(this).css('opacity','1');
+//     $(this).next('span.text-content').hide(); 
+// });
+
+
+//   $scope.hover = function() {
+ 
+// //     //$(".jessie").append("<p><b>Jessieeeee!</b></p>");
+// //    $(".jessie").append("<div id = 'julian'>Hello</div>");
+// //     //$('.jessie').append("<background-color: blue");
+// //     $( this ).fadeOut( 100);
+// //     $( this ).fadeIn( 500 );
+
+//     console.log("hello");
+
+
+// // //   $( "li.fade" ).hover(function() {
+// // //   $( this ).fadeOut( 100 );
+// // //   $( this ).fadeIn( 500 );
+//   }
 })
 
-// Add tool tips to anything with a title property
+// click -> append div -> append info to that div
+// css -> can change the background color to black (or transparent)
+
+//Add tool tips to anything with a title property
 $('body').tooltip({
     selector: '[title]'
 });
